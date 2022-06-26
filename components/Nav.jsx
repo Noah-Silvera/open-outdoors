3dcd2c71d107fef5e2d4cd041405e4fad80afd2a
Noah Silvera <noah@super.gd>
2022-06-26 11:56:26 -0700

    convert to using react-flowbite and update nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,71 +6,32 @@ export default function Nav() {
   return (
     <Navbar
       fluid={true}
-      rounded={true}
+      className={"!py-0 !pl-0 " + styles["nav-background"]}
     >
-      <Navbar.Brand href="https://flowbite.com/">
+      <Navbar.Brand href="/">
         <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
+          src="/logo.png"
+          className="mr-3 h-24"
+          alt="Open (out)Doors"
         />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite
+        <span className="self-center whitespace-nowrap text-2xl sm:text-3xl font-semibold text-primary-light">
+          Open (out)Doors
         </span>
       </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
+      <div className={styles.toggle}>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse >
         <Navbar.Link
-          href="/navbars"
-          active={true}
+          href="#"
+
         >
-          Home
+          <p className="text-primary-light md:text-primary-dark text-2xl">About</p>
         </Navbar.Link>
-        <Navbar.Link href="/navbars">
-          About
-        </Navbar.Link>
-        <Navbar.Link href="/navbars">
-          Services
-        </Navbar.Link>
-        <Navbar.Link href="/navbars">
-          Pricing
-        </Navbar.Link>
-        <Navbar.Link href="/navbars">
-          Contact
+        <Navbar.Link href="#">
+          <p className="text-primary-light md:text-primary-dark text-2xl pb-2 md:pb-0">Gear Swap</p>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-  )
-  return (
-    <nav className={"border-gray-200 header-font pr-5 md:pr-10 " + styles["nav-background"]}>
-      <div className="flex flex-wrap justify-between items-center">
-        <Link href="/">
-          <a className="flex items-center">
-              <img src="/logo.png" className="mr-3 h-24" alt="Open (out)Doors" />
-              <span className="self-center text-2xl sm:text-3xl font-semibold whitespace-nowrap text-primary-light">Open (out)Doors</span>
-          </a>
-        </Link>
-        <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-          <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-          <ul className="flex flex-col mt-4 md:flex-row pb-2 md:pb-0 md:space-x-8 md:mt-0 md:text-sm md:font-medium text-primary-light md:text-primary-dark">
-            <li>
-              <Link href="#">
-                <a className="block py-2 pr-4 pl-3  md:p-0 text-2xl hover:text-blue-700">About</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <a className="block py-2 pr-4 pl-3  md:p-0 text-2xl hover:text-blue-700">Gear Swap</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
   )
 }
