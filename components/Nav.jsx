fbea10b7ecf5575f44031ac39ccffd35d662df49
Noah Silvera <noah@super.gd>
2022-07-06 13:16:58 -0700

    Basic homepage with banner

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,36 +3,34 @@ import { Navbar } from 'flowbite-react'
 
 export default function Nav() {
   return (
-    <div className='pb-24'>
-      <Navbar
-        fluid={true}
-        className={"!py-0 !pl-0 header-font fixed w-screen opacity-80" + " " + styles["nav-background"]}
-      >
-        <Navbar.Brand href="/">
-          <img
-            src="/logo.png"
-            className="mr-3 h-24"
-            alt="Open (out)Doors"
-          />
-          <span className="self-center whitespace-nowrap text-2xl sm:text-3xl font-semibold text-primary-light">
-            Open (out)Doors
-          </span>
-        </Navbar.Brand>
-        <div className={styles.toggle}>
-          <Navbar.Toggle />
-        </div>
-        <Navbar.Collapse >
-          <Navbar.Link
-            href="/about"
+    <Navbar
+      fluid={true}
+      className={"!py-0 !pl-0 header-font fixed w-screen opacity-80" + " " + styles["nav-background"]}
+    >
+      <Navbar.Brand href="/">
+        <img
+          src="/logo.png"
+          className="mr-3 h-24 opacity-100"
+          alt="Open (out)Doors"
+        />
+        <span className="self-center whitespace-nowrap text-2xl sm:text-3xl font-semibold text-primary-light">
+          Open (out)Doors
+        </span>
+      </Navbar.Brand>
+      <div className={styles.toggle}>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse >
+        <Navbar.Link
+          href="/about"
 
-          >
-            <p className="text-primary-light md:text-primary-dark text-2xl">About</p>
-          </Navbar.Link>
-          <Navbar.Link href="/gear-swap">
-            <p className="text-primary-light md:text-primary-dark text-2xl pb-2 md:pb-0">Gear Swap</p>
-          </Navbar.Link>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+        >
+          <p className="text-primary-light md:text-primary-dark text-2xl">About</p>
+        </Navbar.Link>
+        <Navbar.Link href="/gear-swap">
+          <p className="text-primary-light md:text-primary-dark text-2xl pb-2 md:pb-0">Gear Swap</p>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,26 @@ export default function Home() {
       </Head>
       <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js" async></script>
       <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet'></link>
-      <Nav/>
+      <div className='pb-24 sm:pb-0'>
+        <Nav/>
+      </div>
       <div className="min-h-screen bg-tertiary-light">
-
+        <img
+          src="/banner-mobile.jpg"
+          className="max-h-96 w-full md:w-3/5 md:max-w-2xl mx-auto sm:hidden"
+          alt="Open (out)Doors"
+        />
+        <img
+          src="/banner.jpg"
+          className="max-h-96 w-full md:w-3/5 md:max-w-2xl mx-auto hidden sm:block"
+        />
+        <div className='bg-white h-full pt-2 sm:pt-10'>
+          <hr className='bg-secondary-light h-1 w-11/12 mx-auto border-0 hidden sm:block mb-10'/>
+          <div className='w-5/6 sm:w-4/5 md:w-3/5 md:max-w-2xl mx-auto space-y-5'>
+            <h1 className='header-font hidden sm:block'>Open (out)Doors</h1>
+            <p className='text-xl body-font'>Open Outdoors is a Queer led organization that seeks to minimize the financial and logistical barriers to accessing the outdoors for groups that have been historically isolated from the outdoors industry.</p>
+          </div>
+        </div>
       </div>
     </>
   )
