090af81ddb23967cfa07cf64ff1ec14ab6744a5c
Noah Silvera <noah@super.gd>
2022-07-10 13:10:36 -0700

    improve and expand homepage

--- pages/index.js
+++ pages/index.js
@@ -39,19 +39,27 @@ export default function Home() {
               </LeftSideText>
               <RightSideText className="bg-white-alt py-4">
                   <p>
-                    We consist of a few people excited to share their knowledge and resources to help queer people and other marginalized groups get outside and hike, camp, cycle, and more.
+                    We consist of a few people excited to help queer people and other marginalized groups get outside, build relationships, and share knowledge and resources. One of our aims is to minimize financial and logistical barriers to accessing the outdoors by creating community gear share networks and building up a library of gear to lend and give away.
                   </p>
+              </RightSideText>
+              <LeftSideText className="py-4">
+                <p>We also recognize the power of representation in enabling access to space, and want to normalize the inclusion of diverse communities, bodies, and skill levels that been historically excluded from the outdoors industry.</p>
+              </LeftSideText>
+              <RightSideText className="bg-white-alt py-4">
+                <p>
+                  We are a non-hierarchical organization, which for us means actively working to provide the resources and space to enable anyone involved to contribute, and recognizing the contributions of all involved, not just the organizers or leaders.
+                </p>
               </RightSideText>
               <LeftSideText className="py-4">
                   <p>{"This organization is still in it's infancy but our dreams include:"}</p>
                   <ul className='list-disc ml-7 space-y-2 pt-3'>
+                    <li>Gear sharing networks and swaps</li>
                     <li>Coordinating gear donations from major companies</li>
                     <li>Workshops and educational opportunities</li>
                     <li>Guided trips and excursions</li>
                     <li>Community building events</li>
                     <li>Rideshare networks</li>
                     <li>Adventure based communication channels</li>
-                    <li>Gear sharing networks and swaps</li>
                     <li>And more!</li>
                   </ul>
               </LeftSideText>
