9a2cb0bcca8f19a5678d330a4c9bbce60ba2b924
Noah Silvera <noah@super.gd>
2022-09-29 13:26:14 -0700

    Improve styling of gear library banner for mobile users

--- pages/gear-library.js
+++ pages/gear-library.js
@@ -13,11 +13,11 @@ export default function GearLibrary({ content }) {
       </div>
       <main className="min-h-screen" styles={styles["main-container"]}>
         <header>
-          <h1 className='header-font text-center mx-auto pb-10 pt-36 sm:pt-24 md:pt-20 bg-tertiary-light'>Gear Library</h1>
+          <h1 className='header-font text-center mx-auto pb-10 pt-24 sm:pt-22 md:pt-20 bg-tertiary-light'>Gear Library</h1>
           <div className='bg-tertiary-light/70'>
             <p className='text-center text-xl px-4 py-5 max-w-4xl mx-auto'>Aquiring outdoor gear can be a significant barrier for like camping, cycling, hiking, and more. We maintain a library of gear that can be taken out for trips to help make them possible!</p>
           </div>
-          <p className={['text-center py-2 px-3 bg-secondary-dark/80 text-2xl text-white', styles["contact-banner"]].join(" ")}>Contact us at <a className='underline' href="mailto:openoutdoors.victoria@gmail.com">openoutdoors.victoria@gmail.com</a> to borrow gear!</p>
+          <p className={['text-center py-2 px-3 bg-secondary-dark/80 text-md sm:text-lg md:text-2xl text-white', styles["contact-banner"]].join(" ")}>Contact us at <a className='underline' href="mailto:openoutdoors.victoria@gmail.com">openoutdoors.victoria@gmail.com</a> to borrow gear!</p>
         </header>
         <div className='bg-white-alt/70 h-screen'>
           <div className={[styles['gear-library-grid'], "mx-auto p-10"].join(" ")}>
