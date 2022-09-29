1c7f4b1b112c61cf61d2a89a0270b8b90cde3bb0
Noah Silvera <noah@super.gd>
2022-09-29 16:17:50 -0700

    Test transition thresholds

--- components/utils.js
+++ components/utils.js
@@ -2,12 +2,11 @@ export function shiftUpOnScroll(elem) {
   var lastScrollTop;
   window.addEventListener('scroll',function(){
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    let transitionThreshold = 30
-    if((scrollTop - lastScrollTop) > transitionThreshold){
+    if((scrollTop - lastScrollTop) > 25){
       elem.className = elem.className.replaceAll(" is-visible", "")
       elem.className += " is-hidden"
     }
-    else if(((lastScrollTop - scrollTop) > transitionThreshold) || scrollTop == 0){
+    else if(((lastScrollTop - scrollTop) > 60) || scrollTop == 0){
       elem.className = elem.className.replaceAll(" is-hidden", "")
       elem.className += " is-visible"
     }
