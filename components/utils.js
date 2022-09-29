4651f538b8cd73c9065fea5465953d8daa32a7d6
Noah Silvera <noah@super.gd>
2022-09-29 16:31:06 -0700

    Always show nav if close to top of screen

--- components/utils.js
+++ components/utils.js
@@ -2,11 +2,11 @@ export function shiftUpOnScroll(elem) {
   var lastScrollTop;
   window.addEventListener('scroll',function(){
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if((scrollTop - lastScrollTop) > 25){
+    if((scrollTop - lastScrollTop) > 25 && scrollTop > 200){
       elem.className = elem.className.replaceAll(" is-visible", "")
       elem.className += " is-hidden"
     }
-    else if(((lastScrollTop - scrollTop) > 60) || scrollTop == 0){
+    else if(((lastScrollTop - scrollTop) > 60) || scrollTop < 50){
       elem.className = elem.className.replaceAll(" is-hidden", "")
       elem.className += " is-visible"
     }
