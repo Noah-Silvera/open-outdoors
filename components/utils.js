f8ca2d5318543d4aa45f12cce2e739cf23b62f96
Noah Silvera <noah@super.gd>
2022-09-29 16:15:26 -0700

    Smooth out nav bar hiding transition

--- components/utils.js
+++ components/utils.js
@@ -2,11 +2,14 @@ export function shiftUpOnScroll(elem) {
   var lastScrollTop;
   window.addEventListener('scroll',function(){
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if((scrollTop - lastScrollTop) > 50){
+    let transitionThreshold = 30
+    if((scrollTop - lastScrollTop) > transitionThreshold){
+      elem.className = elem.className.replaceAll(" is-visible", "")
       elem.className += " is-hidden"
     }
-    else if(((lastScrollTop - scrollTop) > 50) || scrollTop == 0){
+    else if(((lastScrollTop - scrollTop) > transitionThreshold) || scrollTop == 0){
       elem.className = elem.className.replaceAll(" is-hidden", "")
+      elem.className += " is-visible"
     }
     lastScrollTop = scrollTop;
   });
