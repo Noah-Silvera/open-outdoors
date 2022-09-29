bd3d8b15cc3175a9c42efdbc6cd6016c56a2729c
Noah Silvera <noah@super.gd>
2022-09-29 15:54:12 -0700

    Prevent contact text from destroying page styles

--- pages/contact.js
+++ pages/contact.js
@@ -10,7 +10,7 @@ export default function GearLibrary({ content }) {
       </div>
       <main className="min-h-screen">
         <h1 className='header-font text-center mx-auto py-12 bg-tertiary-light'>Contact Us</h1>
-        <div className='text-center py-20 px-5 text-3xl'>
+        <div className='text-center py-20 px-5 text-xl sm:text-2xl md:text-3xl'>
           <p>You can contact us at <a className='underline' href="mailto:openoutdoors.victoria@gmail.com">openoutdoors.victoria@gmail.com</a> or message us on <a className='underline' href='https://www.instagram.com/open.outdoors/' target="_blank" rel="noreferrer" aria-label="Instagram">instagram!</a></p>
         </div>
       </main>
