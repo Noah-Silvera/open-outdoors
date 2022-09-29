182f6c7fc1a3d7dd4032724557e99505074f28ab
Noah Silvera <noah@super.gd>
2022-09-29 13:20:07 -0700

    Improve gear library description.

--- pages/gear-library.js
+++ pages/gear-library.js
@@ -15,7 +15,7 @@ export default function GearLibrary({ content }) {
         <header>
           <h1 className='header-font text-center mx-auto pb-10 pt-36 sm:pt-24 md:pt-20 bg-tertiary-light'>Gear Library</h1>
           <div className='bg-tertiary-light/70'>
-            <p className='text-center text-xl px-4 py-5 max-w-xl mx-auto'>Aquiring outdoor gear can be a significant barrier to being able to activities like camping, cycling, hiking, and more. We maintain a library of gear that can be taken out for trips to help make them possible!</p>
+            <p className='text-center text-xl px-4 py-5 max-w-4xl mx-auto'>Aquiring outdoor gear can be a significant barrier for like camping, cycling, hiking, and more. We maintain a library of gear that can be taken out for trips to help make them possible!</p>
           </div>
           <p className={['text-center py-2 px-3 bg-secondary-dark/80 text-2xl text-white', styles["contact-banner"]].join(" ")}>Contact us at <a className='underline' href="mailto:openoutdoors.victoria@gmail.com">openoutdoors.victoria@gmail.com</a> to borrow gear!</p>
         </header>
