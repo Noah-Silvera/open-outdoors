cec8ef5656e925b7ea520dac27e96c5c594f5e05
Noah Silvera <noah@super.gd>
2022-10-18 23:20:03 -0700

    Don't add dozens of classnames to dom element

--- components/utils.js
+++ components/utils.js
@@ -3,12 +3,12 @@ export function shiftUpOnScroll(elem) {
   window.addEventListener('scroll',function(){
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if((scrollTop - lastScrollTop) > 25 && scrollTop > 200){
-      elem.className = elem.className.replaceAll(" is-visible", "")
-      elem.className += " is-hidden"
+      elem.classList.remove("is-visible")
+      elem.classList.add("is-hidden")
     }
     else if(((lastScrollTop - scrollTop) > 60) || scrollTop < 50){
-      elem.className = elem.className.replaceAll(" is-hidden", "")
-      elem.className += " is-visible"
+      elem.classList.remove("is-hidden")
+      elem.classList.add("is-visible")
     }
     lastScrollTop = scrollTop;
   });
