62b2dc0e8abb58da19e057255bbfb716440e5bf2
Noah Silvera <noah@super.gd>
2022-10-19 15:49:05 -0700

    Update contact email

--- pages/contact.js
+++ pages/contact.js
@@ -3,7 +3,7 @@ export default function Contact() {
     <main className="min-h-screen">
       <h1 className='header-font text-center mx-auto py-5 md:py-10 bg-tertiary-light'>Contact Us</h1>
       <div className='text-center py-20 px-5 text-xl sm:text-2xl md:text-3xl'>
-        <p>You can contact us at <a className='underline' href="mailto:openoutdoors.victoria@gmail.com">openoutdoors.victoria@gmail.com</a> or message us on <a className='underline' href='https://www.instagram.com/open.outdoors/' target="_blank" rel="noreferrer" aria-label="Instagram">instagram!</a></p>
+        <p>You can contact us at <a className='underline' href="mailto:info@openoutdoorsvictoria.ca">info@openoutdoorsvictoria.ca</a> or message us on <a className='underline' href='https://www.instagram.com/open.outdoors/' target="_blank" rel="noreferrer" aria-label="Instagram">instagram!</a></p>
       </div>
     </main>
   )
