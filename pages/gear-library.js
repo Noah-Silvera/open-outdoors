581dca60fc2124ead3c461bffb50bb58613ba3e1
Noah Silvera <noah@super.gd>
2022-10-18 23:40:18 -0700

    Add "All" option to gear library types

--- pages/gear-library.js
+++ pages/gear-library.js
@@ -47,12 +47,16 @@ function GearItem({gearForLoan, ...props}) {
 }
 
 function TypeRadioButtons({types, onTypeSelect, selectedType}) {
+  let typesWithAllOption = ["All", ...types]
+
   return (
     <div className='flex sm:justify-center px-4 py-5 sm:py-3 text-2xl sm:text-xl bg-tertiary-light w-full sm:px-5 sm:px-5 md:px-10'>
       <div className='flex flex-col sm:flex-row flex-wrap gap-y-4 gap-x-3'>
-        {types.map((type, index) => {
+        {typesWithAllOption.map((type, index) => {
           let idAndVal = type.replace(/ /g,"_").toLowerCase();
-          let isSelected = selectedType == type;
+          let isSelected = type == "All" ?
+            selectedType == null :
+            selectedType == type;
 
           return (
             <div key={index} className={classNames("py-2", "px-3", "rounded-2xl", "flex", "items-center", {"bg-secondary-dark/20": isSelected})}>
@@ -61,7 +65,7 @@ function TypeRadioButtons({types, onTypeSelect, selectedType}) {
                 name="gear_types"
                 value={idAndVal}
                 checked={isSelected}
-                onChange={() => onTypeSelect(type)}
+                onChange={() => onTypeSelect(type == "All" ? null : type)}
               />
               <label htmlFor={idAndVal} className="ml-2 grow">{type} </label>
             </div>
