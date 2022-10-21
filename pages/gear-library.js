a405ea36a74a0ea676e7c9270b7af09328426c7b
Noah Silvera <noah@super.gd>
2022-10-20 18:20:05 -0700

    Move GearItem into separate file

--- pages/gear-library.js
+++ pages/gear-library.js
@@ -1,50 +1,10 @@
 import { createClient } from 'contentful'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import styles from '../styles/GearLibrary.module.scss'
 import { useEffect, useRef, useState } from 'react'
 import { shiftUpOnScroll } from '../components/utils';
-import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { Radio } from 'flowbite-react';
 import classNames from 'classnames';
-
-function GearItem({gearForLoan, ...props}) {
-  let bookedDates = gearForLoan.bookedDates?.filter((bookedDate) => !!bookedDate.fields?.length > 0) || []
-  return (
-  <section className={[styles['gear-item'], "border shadow-sm rounded-md bg-white-alt/10 flex flex-col"].join(" ")} key={props.index}>
-    <Splide>
-      {gearForLoan.images.map((contentfulImage, idx) => {
-        return (
-          <SplideSlide key={idx}>
-            <img
-              src={contentfulImage.fields.file.url}
-              alt={contentfulImage.fields.description}
-              className=' mb-2 md:mb-3 rounded-tl-md rounded-tr-md'/>
-          </SplideSlide>
-        )
-      })}
-    </Splide>
-    <h2 className='text-center'>{gearForLoan.title}</h2>
-    <div className={`px-5 py-2 md:py-3 text-center text-lg ${styles['gear-description']}`}>
-      {documentToReactComponents(gearForLoan.description)}
-    </div>
-    <div className='mx-5 mb-2 grow flex flex-col justify-end'>
-      <h3 className='text-lg md:text-xl font-medium'>Bookings:</h3>
-      {bookedDates.length == 0 ?
-        <p className='text-lg md:text-xl my-2'>No Bookings</p> :
-        bookedDates.map((bookedDate, idx) => {
-          return (
-            <div className='flex flex-row text-lg md:text-xl my-2' key={idx}>
-              <p className='underline'>{new Date(bookedDate.fields.startDate).toDateString()}</p>
-              <p className='mx-2 font-semibold'>to</p>
-              <p className='underline'>{new Date(bookedDate.fields.endDate).toDateString()}</p>
-            </div>
-          )
-        })
-      }
-    </div>
-  </section>
-  )
-}
+import GearItem from '../components/GearItem';
 
 function FilterPanel({ children }) {
   const [isOpen, setIsOpen] = useState(false)
