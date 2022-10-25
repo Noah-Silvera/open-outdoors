0d6a7c19a0dc627ca647ef546675f352f5380dd3
Noah Silvera <noah@super.gd>
2022-10-24 20:55:35 -0700

    Split GearItem input multiple components

--- components/GearItem.jsx
+++ components/GearItem.jsx
@@ -11,50 +11,17 @@ const humanizeDateInputString = (inputDateString) => {
   return match[1]
 }
 
-export default function GearItem({gearForLoan, ...props}) {
+function BorrowForm({gearIdentifier}) {
   let [startDate, setStartDate] = useState("")
   let [endDate, setEndDate] = useState("")
 
   let dateRange = startDate && endDate ? `${humanizeDateInputString(startDate)} to ${humanizeDateInputString(endDate)}` : "<ENTER DATES HERE>"
-  let defaultBorrowMessage = `Hi!
-I would like to borrow the "${gearForLoan.title}" for the following dates:
-${dateRange}`
 
-  let bookedDates = gearForLoan.bookedDates?.filter((bookedDate) => !!bookedDate.fields?.length > 0) || []
+  let defaultBorrowMessage = `Hi!
+  I would like to borrow the "${gearIdentifier}" for the following dates:
+  ${dateRange}`
+
   return (
-  <section className={[styles['gear-item'], "border shadow-sm rounded-md bg-white-alt/10 flex flex-col"].join(" ")} key={props.index}>
-    <Splide>
-      {gearForLoan.images.map((contentfulImage, idx) => {
-        return (
-          <SplideSlide key={idx}>
-            <img
-              src={contentfulImage.fields.file.url}
-              alt={contentfulImage.fields.description}
-              className=' mb-2 md:mb-3 rounded-tl-md rounded-tr-md'/>
-          </SplideSlide>
-        )
-      })}
-    </Splide>
-    <h2 className='text-center'>{gearForLoan.title}</h2>
-    <div className={`px-5 py-2 md:py-3 text-center text-lg ${styles['gear-description']}`}>
-      {documentToReactComponents(gearForLoan.description)}
-    </div>
-    <div className='mx-5 mb-2 grow flex flex-col justify-end'>
-      <h3 className='text-lg md:text-xl font-medium'>Bookings:</h3>
-      {bookedDates.length == 0 ?
-        <p className='text-lg md:text-xl my-2'>No Bookings</p> :
-        bookedDates.map((bookedDate, idx) => {
-          return (
-            <div className='flex flex-row text-lg md:text-xl my-2' key={idx}>
-              <p className='underline'>{new Date(bookedDate.fields.startDate).toDateString()}</p>
-              <p className='mx-2 font-semibold'>to</p>
-              <p className='underline'>{new Date(bookedDate.fields.endDate).toDateString()}</p>
-            </div>
-          )
-        })
-      }
-    </div>
-    <div className='border-b-2 border-primary-light mb-6'></div>
     <div className='text-lg'>
       <div className='px-5'>
         <div className='flex flex-row justify-around pb-3'>
@@ -84,6 +51,53 @@ ${dateRange}`
         </a>
       </div>
     </div>
-  </section>
+  )
+}
+
+function Bookings({ bookedDates }) {
+  return (
+    <div className='mx-5 mb-2 grow flex flex-col justify-end'>
+      <h3 className='text-lg md:text-xl font-medium'>Bookings:</h3>
+      {bookedDates.length == 0 ?
+        <p className='text-lg md:text-xl my-2'>No Bookings</p> :
+        bookedDates.map((bookedDate, idx) => {
+          return (
+            <div className='flex flex-row text-lg md:text-xl my-2' key={idx}>
+              <p className='underline'>{new Date(bookedDate.fields.startDate).toDateString()}</p>
+              <p className='mx-2 font-semibold'>to</p>
+              <p className='underline'>{new Date(bookedDate.fields.endDate).toDateString()}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default function GearItem({gearForLoan, ...props}) {
+  let bookedDates = gearForLoan.bookedDates?.filter((bookedDate) => !!bookedDate.fields?.length > 0) || []
+
+  return (
+    <section className={[styles['gear-item'], "border shadow-sm rounded-md bg-white-alt/10 flex flex-col"].join(" ")} key={props.index}>
+      <Splide>
+        {gearForLoan.images.map((contentfulImage, idx) => {
+          return (
+            <SplideSlide key={idx}>
+              <img
+                src={contentfulImage.fields.file.url}
+                alt={contentfulImage.fields.description}
+                className=' mb-2 md:mb-3 rounded-tl-md rounded-tr-md'/>
+            </SplideSlide>
+          )
+        })}
+      </Splide>
+      <h2 className='text-center'>{gearForLoan.title}</h2>
+      <div className={`px-5 py-2 md:py-3 text-center text-lg ${styles['gear-description']}`}>
+        {documentToReactComponents(gearForLoan.description)}
+      </div>
+      <Bookings bookedDates={bookedDates}/>
+      <div className='border-b-2 border-primary-light mb-6'></div>
+      <BorrowForm gearIdentifier={gearForLoan.title}/>
+    </section>
   )
 }
