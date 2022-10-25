432e8419c4606d2c1bc6a3520807b1240aba9ea7
Noah Silvera <noah@super.gd>
2022-10-24 21:23:57 -0700

    Force white footer text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ export default function SiteFooter(){
   return (
     <Footer>
       <div className="w-full gap-8 py-5 px-6 bg-secondary-dark/90 text-white">
-        <Footer.LinkGroup col={false} className="text-white justify-end align-center">
+        <Footer.LinkGroup col={false} className="!text-white justify-end align-center">
           {navLinks.map((navLink, idx) => {
             return (
               <Footer.Link href={navLink["href"]} key={idx} className="mr-4 flex items-center">
@@ -14,7 +14,7 @@ export default function SiteFooter(){
             )
           })}
           <Footer.Link href="https://www.instagram.com/open.outdoors/" target="_blank" aria-label="Instagram">
-            <span className='text-primary-light md:text-primary-dark'><i aria-hidden="true" className="fab fa-instagram fa-2x pr-5 text-white" title="Instagram"></i></span>
+            <span className='text-primary-light md:text-primary-dark'><i aria-hidden="true" className="fab fa-instagram fa-2x pr-5 !text-white" title="Instagram"></i></span>
           </Footer.Link>
         </Footer.LinkGroup>
       </div>
