5c8e1736bf17e4e7fc79743f535246913cbe4d09
Noah Silvera <noah@super.gd>
2022-10-26 11:50:23 -0700

    Move Filter Panel to separate class

---
+++ components/gear_library/FilterPanel.jsx
@@ -0,0 +1,63 @@
+import { CSSTransition } from "react-transition-group"
+import { GlobalPubSub } from '../utils';
+import styles from '../../styles/GearLibrary.module.scss'
+import { useRef, useState } from 'react'
+import classNames from "classnames";
+
+export default function FilterPanel({ children }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const childContainer = useRef(null)
+  const [navBarClosed, setNavBarClosed] = useState(true)
+
+  GlobalPubSub.subscribe("hidden-on-scroll", (args) => {
+    if(args.identifer == "nav") {
+      setNavBarClosed(true)
+    }
+  })
+
+  GlobalPubSub.subscribe("shown-on-scroll", (args) => {
+    if(args.identifer == "nav") {
+      setNavBarClosed(false)
+    }
+  })
+
+  return (
+    <div className={classNames(
+      styles["sticky-filter-panel"],
+      'z-10',
+      {
+        [styles["nav-bar-closed"]] : navBarClosed
+      }
+    )}>
+      <div
+        className='text-2xl px-6 py-3 bg-tertiary-light flex flex-row items-center border-y-2  border-y-tertiary-light/60'
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <p>Filter</p>
+        <i className={classNames(
+          "fas",
+          "grow",
+          "text-right",
+          {"fa-arrow-up": isOpen},
+          {"fa-arrow-down": !isOpen}
+          )}></i>
+      </div>
+      <CSSTransition
+        in={isOpen}
+        nodeRef={childContainer}
+        timeout={300}
+        classNames={{
+          enter: styles["gear-filter-enter"],
+          enterActive: styles["gear-filter-enter-active"],
+          enterDone: styles["gear-filter-enter-done"],
+          exit: styles["gear-filter-exit"],
+          exitActive: styles["gear-filter-exit-active"],
+          exitDone: styles["gear-filter-exit-done"],
+        }}>
+          <div ref={childContainer} className={styles["gear-filter"]}>
+            {children}
+          </div>
+        </CSSTransition>
+    </div>
+  )
+}
--- pages/gear-library.js
+++ pages/gear-library.js
@@ -1,70 +1,10 @@
 import { createClient } from 'contentful'
 import styles from '../styles/GearLibrary.module.scss'
-import { useEffect, useRef, useState } from 'react'
-import { hideOnScroll } from '../components/utils';
+import { useState } from 'react'
 import { Radio } from 'flowbite-react';
 import classNames from 'classnames';
-import GearItem from '../components/GearItem';
-import { CSSTransition } from "react-transition-group"
-import { GlobalPubSub } from '../components/utils';
-
-function FilterPanel({ children }) {
-  const [isOpen, setIsOpen] = useState(false)
-  const childContainer = useRef(null)
-  const [navBarClosed, setNavBarClosed] = useState(true)
-
-  GlobalPubSub.subscribe("hidden-on-scroll", (args) => {
-    if(args.identifer == "nav") {
-      setNavBarClosed(true)
-    }
-  })
-
-  GlobalPubSub.subscribe("shown-on-scroll", (args) => {
-    if(args.identifer == "nav") {
-      setNavBarClosed(false)
-    }
-  })
-
-  return (
-    <div className={classNames(
-      styles["sticky-filter-panel"],
-      'z-10',
-      {
-        [styles["nav-bar-closed"]] : navBarClosed
-      }
-    )}>
-      <div
-        className='text-2xl px-6 py-3 bg-tertiary-light flex flex-row items-center border-y-2  border-y-tertiary-light/60'
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <p>Filter</p>
-        <i className={classNames(
-          "fas",
-          "grow",
-          "text-right",
-          {"fa-arrow-up": isOpen},
-          {"fa-arrow-down": !isOpen}
-          )}></i>
-      </div>
-      <CSSTransition
-        in={isOpen}
-        nodeRef={childContainer}
-        timeout={300}
-        classNames={{
-          enter: styles["gear-filter-enter"],
-          enterActive: styles["gear-filter-enter-active"],
-          enterDone: styles["gear-filter-enter-done"],
-          exit: styles["gear-filter-exit"],
-          exitActive: styles["gear-filter-exit-active"],
-          exitDone: styles["gear-filter-exit-done"],
-        }}>
-          <div ref={childContainer} className={styles["gear-filter"]}>
-            {children}
-          </div>
-        </CSSTransition>
-    </div>
-  )
-}
+import GearItem from '../components/gear_library/GearItem';
+import FilterPanel from "../components/gear_library/FilterPanel"
 
 function TypeRadioButtons({types, onTypeSelect, selectedType}) {
   let typesWithAllOption = ["All", ...types]
