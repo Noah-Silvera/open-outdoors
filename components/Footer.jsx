0756086a1585d111d5554be4324dc12d7151cbab
Noah Silvera <noah@super.gd>
2022-11-20 09:04:44 -0800

    Support more elements in footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,40 @@
 import { Footer } from 'flowbite-react'
 import { navLinks } from '../src/utils'
 
+function sliceIntoChunks(array, chunkSize) {
+  const result = [];
+  let leftover_amount = array.length % chunkSize;
+  let idx = 0
+  let firstChunk = array.slice(idx, leftover_amount)
+  result.push(firstChunk)
+  console.log(result)
+  idx += leftover_amount;
+  while (idx < array.length) {
+    const chunk = array.slice(idx, (idx+chunkSize));
+    result.push(chunk);
+    idx += chunkSize
+  }
+  return result;
+}
+
 export default function SiteFooter(){
   return (
     <Footer className='justify-self-end grow flex flex-col !justify-end'>
-      <div className="w-full gap-8 py-5 px-6 bg-secondary-dark/90 text-white">
-        <Footer.LinkGroup col={false} className="!text-white justify-end align-center">
-          {navLinks.map((navLink, idx) => {
-            return (
-              <Footer.Link href={navLink["href"]} key={idx} className="mr-4 flex items-center">
-                {navLink["text"]}
-              </Footer.Link>
-            )
-          })}
+      <div className="w-full gap-4 py-5 px-6 bg-secondary-dark/90 text-white flex justify-end">
+        {sliceIntoChunks(navLinks, 2).map((navLinkChunk) =>{
+          return (
+            <Footer.LinkGroup col={true} className="!text-white justify-start align-right space-y-2">
+            {navLinkChunk.map((navLink, idx) => {
+              return (
+                  <Footer.Link href={navLink["href"]} key={idx} className="text-center">
+                    {navLink["text"]}
+                  </Footer.Link>
+              )
+            })}
+            </Footer.LinkGroup>
+          )
+        })}
+        <Footer.LinkGroup col={true} className="!text-white justify-center align-center ml-4">
           <Footer.Link href="https://www.instagram.com/open.outdoors/" target="_blank" aria-label="Instagram">
             <span className='text-primary-light md:text-primary-dark'><i aria-hidden="true" className="fab fa-instagram fa-2x pr-5 !text-white" title="Instagram"></i></span>
           </Footer.Link>
