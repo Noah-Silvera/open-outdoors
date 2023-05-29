d7f7d45a434f80daec53114b50872bc636ebb29c
Noah Silvera <noah.a.silvera@gmail.com>
2023-05-29 09:13:26 -0700

    Create basic pages for gear items

---
+++ pages/gear-library/[gearId].js
@@ -0,0 +1,34 @@
+import GearItem from "../../components/gear_library/GearItem";
+import contentfulClient from "../../src/server/contentful_client";
+
+export default function({ gearItem }){
+  return <GearItem gearForLoan={gearItem}/>
+}
+
+export async function getStaticPaths() {
+  let response = await contentfulClient.getEntries({
+    content_type: "gearForLoan",
+    order: "sys.createdAt"
+  })
+
+  return {
+    paths: response.items.map((item) => {
+      return {
+        params: {
+          gearId: item.sys.id
+        }
+      }
+    }),
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  let response = await contentfulClient.getEntry(params.gearId)
+
+  return {
+    props: {
+      gearItem: response.fields
+    }
+  }
+}
