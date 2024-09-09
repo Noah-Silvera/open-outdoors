8669e804eb3b35f16b580556c5600f7312f315f6
Noah Silvera <noah.a.silvera@gmail.com>
2024-09-09 08:45:25 -0400

    Automatically reply when user books gear

--- pages/api/book_dates.js
+++ pages/api/book_dates.js
@@ -1,12 +1,16 @@
 import initContentfulEnvironment from "../../src/server/contentful_management_client"
 import { performRecaptchaCheck } from "../../src/server/recaptcha_utils"
 import * as Sentry from '@sentry/nextjs';
+import { buildEmailContent } from "../../src/server/email_builder";
+import { AUTOMATIC_REQUEST_RESPONSE_EMAIL_CONTENTFUL_ID } from "../../src/server/contentful_management_client";
+import sendgrid from "@sendgrid/mail";
 
 async function handler(req, res) {
   await performRecaptchaCheck(req.body.recaptchaToken)
 
   let startDate = new Date(req.body.startDate)
   let endDate = new Date(req.body.endDate)
+  let gearEntry;
 
   try {
     const contentfulEnvironment = await initContentfulEnvironment()
@@ -43,7 +47,7 @@ async function handler(req, res) {
 
     await bookedDate.publish()
 
-    let gearEntry = await contentfulEnvironment.getEntry(req.body.gearId)
+    gearEntry = await contentfulEnvironment.getEntry(req.body.gearId)
 
     gearEntry.fields.bookedDates = gearEntry.fields.bookedDates || {}
     gearEntry.fields.bookedDates['en-US'] = gearEntry.fields.bookedDates['en-US'] || []
@@ -61,7 +65,26 @@ async function handler(req, res) {
     await gearEntry.publish()
   } catch (error) {
     Sentry.captureException(error);
-    return res.status(error.statusCode || 500).json({ error: "The given dates could not be booked" });  }
+    return res.status(error.statusCode || 500).json({ error: "The given dates could not be booked" });
+  }
+
+  let { subject, htmlEmailBody } = await buildEmailContent(AUTOMATIC_REQUEST_RESPONSE_EMAIL_CONTENTFUL_ID, { name: req.body.fullName, gearDescription: gearEntry.fields.title['en-US'] })
+
+  try {
+    await sendgrid.send({
+      to: req.body.email,
+      from: "info@openoutdoorsvictoria.ca",
+      replyTo: {
+        email: "info@openoutdoorsvictoria.ca",
+        name: "Open Outdoors"
+      },
+      subject: `[Open Outdoors] : ${subject}`,
+      html: htmlEmailBody
+    });
+  } catch (error) {
+    Sentry.captureException(error);
+    return res.status(error.statusCode || 500).json({ error: "Something went wrong with your request. Please try using the contact form without a gear request to let us know." });
+  }
 
   return res.status(200).json({ error: null });
 }
